43b35bcd0c28747e53c140f35c7a083fbea3dfc1
Michael Donahue <mtdonahue1@gmail.com>
2020-03-15 21:35:34 -0700

    refactor: es vs cjs module output

--- rollup.config.js
+++ rollup.config.js
@@ -3,37 +3,39 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    babel({
-      exclude: 'node_modules/**',
-      extensions: ['.ts', '.tsx'],
-    }),
-    resolve({
-      extensions: ['.ts', '.tsx'],
-    }),
-    commonjs(),
-    postcss({
-      modules: true,
-      extract: true,
-    }),
-  ],
-};
+export default [
+  {
+    input: 'src/components/button/index.ts',
+    output: [
+      {
+        dir: 'lib/es/button',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        dir: 'lib/cjs/button',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      babel({
+        exclude: 'node_modules/**',
+        extensions: ['.ts', '.tsx'],
+      }),
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      commonjs(),
+      postcss({
+        modules: true,
+        extract: true,
+        namedExports: true,
+      }),
+    ],
+  },
+];

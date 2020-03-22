a52c77e5e25e8b150808e1c5733b6cad8b46c5af
Michael Donahue <mtdonahue1@gmail.com>
2020-03-21 19:08:03 -0700

    fix: master index file rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,40 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import pkg from './package.json';
 
 export default [
+  {
+    input: 'src/components/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      babel({
+        exclude: 'node_modules/**',
+        extensions: ['.ts', '.tsx'],
+      }),
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      commonjs(),
+      postcss({
+        modules: true,
+      }),
+    ],
+  },
   {
     input: 'src/components/button/index.ts',
     output: [
